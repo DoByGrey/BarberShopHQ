b3ee890123cea9b5d78f63729e2127aed8a936d5
Savinkin Sergey <ssavinkin13@yandex.ru>
2022-06-21 18:14:52 +0300

    Add Visit.erb, add datatimepiker and colorpiker

--- app.rb
+++ app.rb
@@ -12,7 +12,28 @@ end
 class Barber < ActiveRecord::Base
 end	
 
-get '/' do
+before do
 	@barbers = Barber.all
+end	
+
+get '/' do
+	@barbers = Barber.order "created_at Desc"
 	erb :index
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+
+	@user_name = params[:username]
+	@phone 		 = params[:phone]
+	@datetime  = params[:datetime]
+	@master		 = params[:master]
+	@color		 = params[:color]
+	
+
+
+	erb "<h2>Спасибо, вы  записались!</h2>"
 end
